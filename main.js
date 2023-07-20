1228bcbe6c4de46be0386dce8337ce0406b8de32
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-20 09:55:45 +0200

    new file

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-DBService.getAllShows().then(shows => console.log(shows));
+// DBService.getAllShows().then(shows => console.log(shows));
 
-const show1 = {
-    "title": "rick and morty",
-    "author": "justin roiland",
-    "imageUrl": "https://fumettologica.it/wp-content/uploads/2023/01/rick-and-morty-670x377.jpg",
-    "isOver": false,
-    "upVotes": 1,
-    "downVotes": 0,
-    "id": "1"
-}
+// const show1 = {
+//     "title": "rick and morty",
+//     "author": "justin roiland",
+//     "imageUrl": "https://fumettologica.it/wp-content/uploads/2023/01/rick-and-morty-670x377.jpg",
+//     "isOver": false,
+//     "upVotes": 1,
+//     "downVotes": 0,
+//     "id": "1"
+// }
 
-show1.upVotes++;
+// DBService.updateShow(show1).then(show => console.log(show));
 
-updateShow(show1).then();
+const app = new AppController();
+
+app.init();
