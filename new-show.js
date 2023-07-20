60a233fc512f6bdb67cbc937440c7c1f1a3b3e43
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-20 12:38:13 +0200

    Update new-show.js

--- new-show.js
+++ new-show.js
@@ -4,12 +4,21 @@ function sendData(event) {
     const form = document.forms['create'];
     // const title = form['title'].value;
     const formData = new FormData(form);
+
+    let isOverBool;
+
+    if (formData.get('isOver') === 'on') {
+        isOverBool = true;
+    } else {
+        isOverBool = false;
+    }
+
     const newSerie = {
         title: formData.get('title'),
         author: formData.get('author'),
         imageUrl: formData.get('imageUrl'),
-        isOver: formData.get('isOver') === 'on' ? true : false,
-        upVotes: formData.get('upVotes'),
-        downVotes: formData.get('downVotes')
+        isOver: isOverBool,
+        upVotes: 0,
+        downVotes: 0
     }
 }
