f83f7126f0e122ba2c45c16a8de61f76ff887503
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-20 10:04:18 +0200

    Update app-controller.js

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -1,13 +1,15 @@
 class AppController {
     constructor(){
-
+        this.shows = [];
     }
 
     init() {
-        
+        this.render();
     }
 
     render() {
+        const appContainer = document.getElementById('app');
 
+        appContainer.innerHTML = ``;
     }
 }
