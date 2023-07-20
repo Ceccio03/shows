e3d11a827d1b0be9379c6fb5f0ecfd010e59ec32
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-20 09:42:32 +0200

    commit

--- services/db-service.js
+++ services/db-service.js
@@ -1,3 +1,29 @@
 class DBService {
-    
+    static getAllShows() {
+        const url = 'https://64b7ae3421b9aa6eb078ca81.mockapi.io/shows';
+
+        return fetch(url).then((resp) => resp.json());
+    }
+
+    static updateShow(show) {
+        const updateUrl = 'https://64b7ae3421b9aa6eb078ca81.mockapi.io/shows' + show.id;
+
+        return fetch(updateUrl, {
+            method: "put",
+            body: JSON.stringify(show),
+            headers: {"content-type": "application/json"},
+        }).then((resp) => resp.json());
+    }
+
+    static upvote(show) {
+        show.upVotes++;
+
+        this.updateShow(show);
+    }
+
+    static downvote(show) {
+        show.downVotes++;
+        
+        this.updateShow(show);
+    }
 }
